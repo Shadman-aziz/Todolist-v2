9e2c820aadb5700be4246143253c491463a74e5f
Shadman Aziz <97sdmn@gmail.com>
2020-10-01 01:49:51 -0400

    Add files via upload

---
+++ date.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+exports.getDate =function() {
+
+
+let today = new Date();
+let options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+
+};
+
+return today.toLocaleDateString("en-US",options);
+
+}
